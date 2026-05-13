a96e1aa6748e232d0f3344c1ce17ac1cac2048af
alexanderniki <aalexanderniki@gmail.com>
2026-05-13 20:08:24 +0400

    Added text tools in a separate file

---
+++ text_tools.py
@@ -0,0 +1,84 @@
+"""
+Collection of misc tools and helpers wor working with text
+
+    Classes:
+        --
+
+    Functions:
+        wrap(line: str, width: int) -> str
+        truncate(line: str, limit -> int) -> str
+        format_line(line: str) -> str
+
+    Misc variables and constants:
+        --
+"""
+
+
+import textwrap
+
+
+def wrap(line: str, width: int) -> str:
+    """
+    Wraps one line into several ones with width character per each
+
+        Args:
+            line (str): Given line
+            width (int): Number of characters per each line in the result
+
+        Returns:
+            (str): Wrapped line
+    """
+
+    if width > 0:
+        result: str = textwrap.fill(line, width=width, break_long_words=False)
+    else:
+        result = ""
+
+    return result
+
+
+def truncate(line: str, limit: int, truncate_char: str = "…") -> str:
+    """
+    Truncates a line
+
+        Args:
+            line (str): Given line
+            limit (str): Number of characters in the result
+            truncate_char (str): What to place instead of the rest of the line. Default: "…"
+
+        Returns:
+            (str): Truncated line
+    """
+
+    if limit > 0 and len(line) > limit:
+        result: str = line[:limit - 1] + truncate_char
+    else:
+        result = ""
+
+    return result
+
+
+def format_line(line: str, wrap_width: int, truncate_limit: int) -> str:
+    """
+    Wrap and truncate the line
+
+        Args:
+            line (str): Given line
+            wrap_width (int): New line width
+            truncate_limit (int): When to truncate the line
+
+        Returns:
+            (str): Formatted line
+    """
+
+    result: str = line
+
+    if wrap_width < truncate_limit:
+        result = wrap(result, wrap_width)
+        if len(line) > truncate_limit:
+            result = truncate(result, truncate_limit)
+    else:
+        if wrap_width >= truncate_limit:
+            result = truncate(result, truncate_limit)
+
+    return result
